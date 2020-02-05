b925d733eea34eccd975ba4077073c44d9106baa
alna0916 <aliaksei.nastsin@netcracker.com>
2020-02-05 18:09:39 +0300

    feat: 7 sec

--- main/signer.go
+++ main/signer.go
@@ -26,24 +26,19 @@ func main() {
 			fmt.Print(testResult)
 		}),
 	}
-
 	ExecutePipeline(hashSignJobs...)
 }
 
 func ExecutePipeline(jobs ...job) {
 	waitGroup := &sync.WaitGroup{}
-
 	channels := make([]chan interface{}, len(jobs)+1)
-
 	for i := 0; i < len(jobs)+1; i++ {
 		channels[i] = createChannel()
 	}
-
 	for idx, job := range jobs {
 		waitGroup.Add(1)
 		go executePipeline(waitGroup, channels[idx], channels[idx+1], job)
 	}
-
 	waitGroup.Wait()
 }
 
@@ -54,35 +49,59 @@ func executePipeline(waitGroup *sync.WaitGroup, in chan interface{}, out chan in
 }
 
 func SingleHash(in, out chan interface{}) {
+	waitGroupSingleHash := &sync.WaitGroup{}
+	mutex := &sync.Mutex{}
 	for data := range in {
-		dataString := strconv.Itoa(data.(int))
-		hash := DataSignerCrc32(dataString) + "~" + DataSignerCrc32(DataSignerMd5(dataString))
-		out <- hash
+		waitGroupSingleHash.Add(1)
+		go asyncIterationSingleHash(mutex, data, out, waitGroupSingleHash)
 	}
+	waitGroupSingleHash.Wait()
+}
+
+func asyncIterationSingleHash(mutex *sync.Mutex, data interface{}, out chan interface{}, waitGroupSingleHash *sync.WaitGroup) {
+	defer waitGroupSingleHash.Done()
+	dataString := strconv.Itoa(data.(int))
+	md5 := hashMD5(mutex, dataString)
+	firstCrc32 := hashCrc32(dataString)
+	secondCrc32 := hashCrc32(<-md5)
+	hash := <-firstCrc32 + "~" + <-secondCrc32
+	out <- hash
 }
 
 func MultiHash(in, out chan interface{}) {
+	waitGroupMultiHash := &sync.WaitGroup{}
 	for data := range in {
-		dataString := data.(string)
-		buffer := make([]string, 6)
-		for i := 0; i <= 5; i++ {
-			formatInt := strconv.Itoa(i)
-			number := DataSignerCrc32(formatInt + dataString)
-			buffer = append(buffer, number)
-		}
-		finalResult := ""
-		for idx := range buffer {
-			finalResult += buffer[idx]
-		}
-		out <- finalResult
+		waitGroupMultiHash.Add(1)
+		go asyncIterationMultiHash(data, out, waitGroupMultiHash)
 	}
+	waitGroupMultiHash.Wait()
+}
+
+func asyncIterationMultiHash(data interface{}, out chan interface{}, waitGroupMultiHash *sync.WaitGroup) {
+	defer waitGroupMultiHash.Done()
+	dataString := data.(string)
+	buffer := make([]string, 6)
+	for i := 0; i <= 5; i++ {
+		formatInt := strconv.Itoa(i)
+		number := hashCrc32(formatInt + dataString)
+		buffer = append(buffer, <-number)
+	}
+	finalResult := ""
+	for idx := range buffer {
+		finalResult += buffer[idx]
+	}
+	out <- finalResult
 }
 
 func CombineResults(in, out chan interface{}) {
+	//waitGroupCombineResults := &sync.WaitGroup{}
 	data := make([]string, 0)
 	for value := range in {
+		//waitGroupCombineResults.Add(1)
+		//go asyncIterationCombineResults(data, value, waitGroupCombineResults)
 		data = append(data, value.(string))
 	}
+	//waitGroupCombineResults.Wait()
 	sort.Strings(data)
 	finalResult := ""
 	length := len(data)
@@ -94,6 +113,31 @@ func CombineResults(in, out chan interface{}) {
 		}
 	}
 	out <- finalResult
+}
+
+func asyncIterationCombineResults(data []string, value interface{}, waitGroupCombineResults *sync.WaitGroup) {
+	defer waitGroupCombineResults.Done()
+	data = append(data, value.(string))
+}
+
+func hashMD5(mutex *sync.Mutex, dataString string) chan string {
+	result := make(chan string, 1)
+	go func(out chan<- string) {
+		mutex.Lock()
+		md5 := DataSignerMd5(dataString)
+		mutex.Unlock()
+		out <- md5
+	}(result)
+	return result
+}
+
+func hashCrc32(dataString string) chan string {
+	result := make(chan string, 1)
+	go func(out chan<- string) {
+		crc32 := DataSignerCrc32(dataString)
+		out <- crc32
+	}(result)
+	return result
 }
 
 func createChannel() chan interface{} {
